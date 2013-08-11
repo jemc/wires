9d44186dd05011a1f98110e42e0ae5df5d7fd90a
Joe McIlvain <joe.eli.mac@gmail.com>
2013-08-11 14:16:00 -0400

    Test reporter is now jemc-reporter gem

--- spec/clean_spec.rb
+++ spec/clean_spec.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift(File.expand_path("../lib", File.dirname(__FILE__)))
 require 'wires'
 
 require 'wires/test'
-Wires.test_format
+begin require 'jemc/reporter'; rescue LoadError; end
 
 _main = self
 
