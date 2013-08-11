4ae15cdb48d1a953fbff174a074335262e5c1583
Joe McIlvain <joe.eli.mac@gmail.com>
2013-08-11 14:41:57 -0400

    Moved Wires::Convenience to wires/convenience

--- lib/wires.rb
+++ lib/wires.rb
@@ -10,5 +10,6 @@ require 'wires/hub'
 require 'wires/channel'
 require 'wires/time'
 
+require 'wires/convenience'
 include Wires::Convenience # require 'wires/clean' to uninclude Convenience
 
--- lib/wires/channel.rb
+++ lib/wires/channel.rb
@@ -1,49 +1,5 @@
 
 module Wires
-
-  module Convenience
-    
-    def on(events, channels='*', &codeblock)
-      channels = [channels] unless channels.is_a? Array
-      for channel in channels
-        Channel.new(channel).register(events, codeblock)
-      end
-    nil end
-    
-    def fire(event, channel='*') 
-      Channel.new(channel).fire(event, blocking:false)
-    nil end
-    
-    def fire_and_wait(event, channel='*') 
-      Channel.new(channel).fire(event, blocking:true)
-    nil end
-    
-    def Channel(*args) Channel.new(*args) end
-    
-    class << self
-      def prefix_methods(prefix)
-        
-        return unless prefix
-        prefix = prefix.to_s
-        
-        instance_methods.each do |thing|
-          thing = thing.to_s
-          f2 = (prefix+'_'+thing)
-          f2 = (thing[0]=~/[[:lower:]]/) ? f2.underscore : f2.camelcase
-          f2 = f2.to_sym; thing = thing.to_sym
-          alias_method f2, thing
-          remove_method thing
-        end
-        
-      end
-    end
-    
-  end
-
-end
-
-
-module Wires
   
   class Channel
     
