5abe53caef9a51cae5832d1752dc849b2f74ed61
Joe McIlvain <joe.eli.mac@gmail.com>
2013-08-16 22:12:37 -0400

    More performance boost to Channel - memoizing relevant_channels

--- lib/wires/channel.rb
+++ lib/wires/channel.rb
@@ -6,11 +6,6 @@ module Wires
     attr_reader :name
     attr_reader :target_list
     
-    def initialize(name)
-      @name = name
-      @target_list = Set.new
-    nil end
-    
     # Redefine this class method to use an alternate Hub
     def self.hub; Hub; end
     # Don't redefine this instance method!
@@ -30,6 +25,20 @@ module Wires
       @@new_lock.synchronize do
         @@channel_hash ||= Hash.new
         @@channel_hash[args[0]] ||= super(*args, &block)
+      end
+    end
+    
+    def initialize(name)
+      @name = name
+      @target_list = Set.new
+      unless @@channel_hash.empty?
+        _relevant_init
+        @@channel_hash.values.each do |c| 
+          c.send(:_test_relevance, self)
+          _test_relevance c
+        end
+      else
+        @relevant_channels = []
       end
     end
     
@@ -99,38 +108,41 @@ module Wires
       
     nil end
     
-    def relevant_channels
-      return @@channel_hash.values if self==channel_star
-        
-      relevant = [channel_star]
-      my_names = (self.name.is_a? Array) ? self.name : [self.name]
-      my_names.map {|o| (o.respond_to? :channel_name) ? o.channel_name : o.to_s}
+    def _relevant_init
+      @relevant_channels = [@@channel_star]
+      @my_names = (self.name.is_a? Array) ? self.name : [self.name]
+      @my_names.map {|o| (o.respond_to? :channel_name) ? o.channel_name : o.to_s}
               .flatten(1)
+    end
+    
+    def _test_relevance(other_chan)
+      if self==@@channel_star
+        @relevant_channels << other_chan
+        return true
+      end
       
-      for my_name in my_names
+      for my_name in @my_names
         
         if my_name.is_a?(Regexp) then raise TypeError,
           "Cannot fire on Regexp channel: #{self.name}."\
           "  Regexp channels can only used in event handlers." end
         
-        for other_chan in @@channel_hash.values
-          
-          other_name = other_chan.name
-          other_name = (other_name.respond_to? :channel_name) ? \
-                          other_name.channel_name : other_name
-          
-          relevant << other_chan if \
-            if other_name.is_a?(Regexp)
-              my_name =~ other_name
-            else
-              my_name.to_s == other_name.to_s
-            end
-            
-        end
+        other_name = other_chan.name
+        other_name = (other_name.respond_to? :channel_name) ? \
+                        other_name.channel_name : other_name
+        
+        @relevant_channels << other_chan if \
+          if other_name.is_a?(Regexp)
+            my_name =~ other_name
+          else
+            my_name.to_s == other_name.to_s
+          end
         
       end
-      
-      return relevant.uniq
+    end
+    
+    def relevant_channels
+      return @relevant_channels.uniq
     end
     
     # Compare matching with another Channel
