72e71075e09226f585fbfbc03c065fc9e44159a4
Joe McIlvain <joe.eli.mac@gmail.com>
2013-09-19 05:22:55 -0400

    Removed Wires::Duration - just extend Numeric instead

--- lib/wires/core_ext.rb
+++ lib/wires/core_ext.rb
@@ -20,13 +20,38 @@ class ::Numeric
       or instance_methods.include? #{k.first.inspect}
     
     def #{k.last}
-      Wires::Duration.new self * #{v}
+      self * #{v}
     end
     alias #{k.first.inspect} #{k.last.inspect}
     
   end
 CODE
   end
+  
+  # Reads best without arguments:  10.minutes.ago
+  def ago(time = ::Time.now, &block)
+    if block
+      on :time_scheduler_anon, block.object_id do |e| block.call(e) end
+      self.ago(time).fire(:time_scheduler_anon, block.object_id)
+    end
+    time - self
+  end
+
+  # Reads best with argument:  10.minutes.until(time)
+  alias :until :ago
+
+  # Reads best with argument:  10.minutes.since(time)
+  def since(time = ::Time.now, &block)
+    if block
+      on :time_scheduler_anon, block.object_id do |e| block.call(e) end
+      self.since(time).fire(:time_scheduler_anon, block.object_id)
+    end
+    time + self
+  end
+
+  # Reads best without arguments:  10.minutes.from_now
+  alias :from_now :since
+  
   
 end
 
--- spec/core_ext_spec.rb
+++ spec/core_ext_spec.rb
@@ -57,5 +57,54 @@ describe "wires/core_ext::Time" do
   
 end
 
+
+# Duration objects help with syntax sugar and can fire anonymous event blocks
 describe "wires/core_ext::Numeric" do
+  include TimeTester
+  
+  it "can now fire timed anonymous events, given a code block" do
+    
+    var = 'before'
+    0.1.seconds.from_now do 
+      var = 'after'
+    end
+    sleep 0.05
+    var.must_equal 'before'
+    sleep 0.15
+    var.must_equal 'after'
+    
+  end
+  
+  it "can now fire anonymous events at at time related to another time" do
+    var = 'before'
+    0.1.seconds.until(0.2.seconds.from_now) do 
+      var = 'after'
+    end
+    sleep 0.05
+    var.must_equal 'before'
+    sleep 0.15
+    var.must_equal 'after'
+    
+  end
+  
+  it "can now fire timed anonymous events, which don't match with eachother" do
+    
+    fire_count = 20
+    done_count = 0
+    past_events = []
+    
+    for i in 0...fire_count
+      (i*0.01+0.1).seconds.from_now do |event|
+        done_count += 1
+        past_events.wont_include event
+        past_events << event
+      end
+    end
+    
+    sleep (fire_count*0.01+0.2)
+    
+    done_count.must_equal fire_count
+    
+  end
+  
 end
