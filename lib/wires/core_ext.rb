2b7c908f967465f72ace7575a2993eb34fd6c526
Joe McIlvain <joe.eli.mac@gmail.com>
2013-09-19 05:31:45 -0400

    More refactoring and drying of core_ext.rb

--- lib/wires/core_ext.rb
+++ lib/wires/core_ext.rb
@@ -1,8 +1,20 @@
 
+# Reopen the core Time class and add the fire method enabling nifty syntax like:
+# 32.minutes.from_now.fire :event
+class ::Time
+  unless instance_methods.include? :fire
+    def fire(event, channel='*', **kwargs)
+      Wires::TimeScheduler.add(self, event, channel, **kwargs)
+    end
+  end
+end
+
+
 # Reopen the core Numeric class to add syntax sugar for 
 # generating Wires::Duration objects
 class ::Numeric
   
+  # Add Numeric => Numeric time-factor converters
   {
     [:second,     :seconds]    => '1',
     [:minute,     :minutes]    => '60',
@@ -28,39 +40,26 @@ class ::Numeric
 CODE
   end
   
-  # Reads best without arguments:  10.minutes.ago
-  def ago(time = ::Time.now, &block)
-    if block
-      on :time_scheduler_anon, block.object_id do |e| block.call(e) end
-      self.ago(time).fire(:time_scheduler_anon, block.object_id)
+  # Add Numeric => Time converters with implicit anonymous fire
+  {
+    [:until,    :ago]   => '-',
+    [:from_now, :since] => '+',
+  }.each_pair do |k,v|
+    eval <<-CODE
+  unless instance_methods.include? #{k.last.inspect} \
+      or instance_methods.include? #{k.first.inspect}
+    
+    def #{k.last}(time = ::Time.now, &block)
+      if block
+        on :time_scheduler_anon, block.object_id do |e| block.call(e) end
+        self.#{k.last}(time).fire(:time_scheduler_anon, block.object_id)
+      end
+      time #{v} self
     end
-    time - self
+    alias #{k.first.inspect} #{k.last.inspect}
+    
   end
-
-  # Reads best with argument:  10.minutes.until(time)
-  alias :until :ago
-
-  # Reads best with argument:  10.minutes.since(time)
-  def since(time = ::Time.now, &block)
-    if block
-      on :time_scheduler_anon, block.object_id do |e| block.call(e) end
-      self.since(time).fire(:time_scheduler_anon, block.object_id)
-    end
-    time + self
+CODE
   end
-
-  # Reads best without arguments:  10.minutes.from_now
-  alias :from_now :since
   
-  
-end
-
-# Reopen the core Time class and add the fire method enabling nifty syntax like:
-# 32.minutes.from_now.fire :event
-class ::Time
-  unless instance_methods.include? :fire
-    def fire(event, channel='*', **kwargs)
-      Wires::TimeScheduler.add(self, event, channel, **kwargs)
-    end
-  end
 end
