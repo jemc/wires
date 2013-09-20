a99de30c9ce655f0ad4bd1ba9cf8c9dccb9505c6
Joe McIlvain <joe.eli.mac@gmail.com>
2013-09-20 01:31:59 -0400

    Added possibility to opt out of core extensions with Wires::Convenience.core_ext=false

--- lib/wires/core_ext.rb
+++ lib/wires/core_ext.rb
@@ -1,53 +1,65 @@
 
 module Wires
   module Convenience
-    def self.included *args
-      extend_core
-    end
-
-    def self.extend_core
+    
+    @core_ext = true
+    
+    class << self
       
-      # Add Time#fire for timed firing of events
-      ::Time.class_eval do
-        def fire(event, channel='*', **kwargs)
-          Wires::TimeScheduler.add(self, event, channel, **kwargs)
+      # Set this attribute to false to disable core_ext on include
+      attr_accessor :core_ext
+      
+      # Call extend_core on include unless attribute is set to false
+      def included(*args)
+        super
+        self.extend_core if @core_ext
+      end
+      
+      # Add methods to ::Time and ::Numeric
+      def extend_core
+        # Add Time#fire for timed firing of events
+        ::Time.class_eval do
+          def fire(event, channel='*', **kwargs)
+            Wires::TimeScheduler.add(self, event, channel, **kwargs)
+          end
+        end
+          
+        # Add Numeric => Numeric time-factor converters
+        {
+          [:second,     :seconds]    => '1',
+          [:minute,     :minutes]    => '60',
+          [:hour,       :hours]      => '3600',
+          [:day,        :days]       => '24.hours',
+          [:week,       :weeks]      => '7.days',
+          [:fortnight,  :fortnights] => '2.weeks',
+        }.each_pair do |k,v|
+          ::Numeric.class_eval <<-CODE
+            def #{k.last}
+              self * #{v}
+            end
+            alias #{k.first.inspect} #{k.last.inspect}
+          CODE
+        end
+        
+        # Add Numeric => Time converters with implicit anonymous fire
+        {
+          [:from_now, :since] => '+',
+          [:until,    :ago]   => '-',
+        }.each_pair do |k,v|
+          ::Numeric.class_eval <<-CODE
+            def #{k.last}(time = ::Time.now, &block)
+              if block
+                on :time_scheduler_anon, block.object_id do |e| block.call(e) end
+                self.#{k.last}(time).fire(:time_scheduler_anon, block.object_id)
+              end
+              time #{v} self
+            end
+            alias #{k.first.inspect} #{k.last.inspect}
+          CODE
         end
       end
-        
-      # Add Numeric => Numeric time-factor converters
-      {
-        [:second,     :seconds]    => '1',
-        [:minute,     :minutes]    => '60',
-        [:hour,       :hours]      => '3600',
-        [:day,        :days]       => '24.hours',
-        [:week,       :weeks]      => '7.days',
-        [:fortnight,  :fortnights] => '2.weeks',
-      }.each_pair do |k,v|
-        ::Numeric.class_eval <<-CODE
-          def #{k.last}
-            self * #{v}
-          end
-          alias #{k.first.inspect} #{k.last.inspect}
-        CODE
-      end
-      
-      # Add Numeric => Time converters with implicit anonymous fire
-      {
-        [:from_now, :since] => '+',
-        [:until,    :ago]   => '-',
-      }.each_pair do |k,v|
-        ::Numeric.class_eval <<-CODE
-          def #{k.last}(time = ::Time.now, &block)
-            if block
-              on :time_scheduler_anon, block.object_id do |e| block.call(e) end
-              self.#{k.last}(time).fire(:time_scheduler_anon, block.object_id)
-            end
-            time #{v} self
-          end
-          alias #{k.first.inspect} #{k.last.inspect}
-        CODE
-      end
       
     end
+    
   end
 end
--- sandbox.rb
+++ sandbox.rb
@@ -1,6 +1,9 @@
-# $LOAD_PATH.unshift(File.expand_path("./lib", File.dirname(__FILE__)))
-# require 'wires'
-# include Wires
+$LOAD_PATH.unshift(File.expand_path("./lib", File.dirname(__FILE__)))
+require 'wires'
+# Wires::Convenience.core_ext=false
+include Wires::Convenience
 
 # require 'wires/test'
 # begin require 'jemc/reporter'; rescue LoadError; end
+
+p 5.minutes
