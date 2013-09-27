41c73893f9f1cf4f70551389c2270b7e52a620e5
Joe McIlvain <joe.eli.mac@gmail.com>
2013-09-26 22:42:28 -0400

    Added test for Router::Simple

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -22,9 +22,23 @@ describe Wires::Router::Simple do
   
   # Clean out channel list between each test
   def setup
+    Wires::Channel.router = Wires::Router::Simple
     Wires::Channel.router.clear_channels
   end
   
-  it ""
+  def teardown
+    Wires::Channel.router.clear_channels
+    Wires::Channel.router = Wires::Router::Default
+  end
+  
+  it "routes only on exact object matches" do
+    channels = ['channel',   'Channel',  'CHANNEL',
+                :channel,    :Channel,   :CHANNEL,
+                /channel/,   /Channel/,  /CHANNEL/,
+               ['channel'], [:channel], [/channel/],
+                 self,        Object.new, Hash.new].map {|x| Wires::Channel[x]}
+    
+    channels.each { |x| x.receivers.must_equal [x] }
+  end
   
 end
