8a97452a45f47c7cde81454f02eb8c225d04407f
Joe McIlvain <joe.eli.mac@gmail.com>
2013-11-23 01:22:12 -0500

    Move ::Time core extensions out of class_eval

--- lib/wires/core_ext/time.rb
+++ lib/wires/core_ext/time.rb
@@ -1,7 +1,7 @@
 
 # Add Time#fire for timed firing of events
-::Time.class_eval <<-CODE
-  def fire(event, channel='*', **kwargs)
-    #{Wires::TimeScheduler}.add(self, event, channel, **kwargs)
+class ::Time
+  def fire(events, channel, **kwargs)
+    Wires::TimeScheduler.add(self, events, channel, **kwargs)
   end
-CODE
+end
