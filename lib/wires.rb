bbe57eeeedf3d083d12053c0390d6bd937279911
Joe McIlvain <joe.eli.mac@gmail.com>
2013-12-08 15:00:47 -0500

    Fix comment referencing 'wires/clean', which is now known as 'wires/base'

--- lib/wires.rb
+++ lib/wires.rb
@@ -1,6 +1,6 @@
 
-# Require all core wires functionality through 'wires/clean'
-# If you don't consent to the core extensions, require 'wires/clean' directly
+# Require all core wires functionality through 'wires/base'
+# If you don't consent to the core extensions, require 'wires/base' directly
 require_relative 'wires/base'
 
 # Add core extensions for syntax sugar and ease
