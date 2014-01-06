ada6275bfa20482f8acc3e23b094c84180a8e635
Joe McIlvain <joe.eli.mac@gmail.com>
2014-01-05 23:56:27 -0500

    Don't use to_a/each (splat) on the channels argument; if it is a socket, it blocks indefinitely... gross.

--- lib/wires/base/convenience.rb
+++ lib/wires/base/convenience.rb
@@ -4,7 +4,8 @@ module Wires.current_network::Namespace
   module Convenience
     
     def on(events, channels=self, &codeblock)
-      [*channels].each do |channel|
+      channels = [channels] unless channels.is_a? Array
+      channels.each do |channel|
         channel=Channel.new(channel) unless channel.is_a? Channel
         
         channel.register(*events, &codeblock)
@@ -12,16 +13,14 @@ module Wires.current_network::Namespace
       codeblock
     end
     
-    def fire(event, channels=self, **kwargs)
-      [*channels].each do |channel|
-        channel = Channel.new(channel) unless channel.is_a? Channel
-        
-        if kwargs[:time] or (kwargs[:count] and kwargs[:count]!=1)
-          time = kwargs.delete(:time) or Time.now
-          TimeScheduler.add(time, event, channel, **kwargs)
-        else
-          channel.fire(event, **kwargs)
-        end
+    def fire(event, channel, **kwargs)
+      channel = Channel.new(channel) unless channel.is_a? Channel
+      
+      if kwargs[:time] or (kwargs[:count] and kwargs[:count]!=1)
+        time = kwargs.delete(:time) or Time.now
+        TimeScheduler.add(time, event, channel, **kwargs)
+      else
+        channel.fire(event, **kwargs)
       end
     nil end
     
