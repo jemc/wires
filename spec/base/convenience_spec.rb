94e7b97462d286dcd8a70932dda5720e88d8d50c
Joe McIlvain <joe.eli.mac@gmail.com>
2014-02-08 22:17:19 -0800

    Made convenience_spec test forwarding instead of testing end behaviour
    --HG--
    extra : amend_source : 5a46d93936dec03bfb9d846569573bf9aac6d416

--- spec/base/convenience_spec.rb
+++ spec/base/convenience_spec.rb
@@ -7,78 +7,93 @@ require 'spec_helper'
 describe Wires::Convenience do
   subject { Object.new.extend Wires::Convenience }
   
-  describe "#fire and #on" do
-    let(:on_method)   { Proc.new { |e,c,&blk| subject.on   e,c,&blk } }
-    let(:fire_method) { Proc.new { |e,c,**kw| subject.fire e,c,**kw } }
-    
-    it_behaves_like "a variable-channel fire method"
-    it_behaves_like "a non-blocking fire method"
-    
-    it "fires to self by default" do
-      extend Wires::Convenience
-      received = false
-      on(:sym, self) { |e,c| received = true }
-      fire :sym
-      sleep 0.15
-      received.should eq true
-    end
-    
-    it "listens on self by default" do
-      extend Wires::Convenience
-      received = false
-      on(:sym) { |e,c| received = true }
-      fire :sym, self
-      sleep 0.15
-      received.should eq true
-    end
-    # it "can do time related stuff as well"
-  end
+  let(:event) { Wires::Event.new }
+  let(:channel_name) { 'test' }
+  let(:channel) { Wires::Channel[channel_name] }
+  let(:a_proc) { Proc.new { nil } }
   
-  describe "#fire! and #on" do
-    let(:on_method)   { Proc.new { |e,c,&blk| subject.on    e,c,&blk } }
-    let(:fire_method) { Proc.new { |e,c,**kw| subject.fire! e,c,**kw } }
-    
-    it_behaves_like "a variable-channel fire method"
-    it_behaves_like "a blocking fire method"
-    
-    it "fires to self by default" do
-      extend Wires::Convenience
-      received = false
-      on(:sym, self) { |e,c| received = true }
-      fire! :sym
-      received.should eq true
+  describe "#on" do
+    it "forwards to Channel[self].register by default" do
+      expect(Wires::Channel[subject]).to receive(:register).with(event, &a_proc)
+      subject.on(event, &a_proc)
     end
     
-    it "listens on self by default" do
-      extend Wires::Convenience
-      received = false
-      on(:sym) { |e,c| received = true }
-      fire! :sym, self
-      received.should eq true
+    it "forwards to the Channel when given the channel object" do
+      expect(channel).to receive(:register).with(event, &a_proc)
+      subject.on(event, channel, &a_proc)
+    end
+    
+    it "forwards to the Channel when given the channel name" do
+      expect(channel).to receive(:register).with(event, &a_proc)
+      subject.on(event, channel_name, &a_proc)
+    end
+    
+    it "forwards to multiple Channels when given a channel object array" do
+      expect(Wires::Channel['1']).to receive(:register).with(event, &a_proc)
+      expect(Wires::Channel['2']).to receive(:register).with(event, &a_proc)
+      expect(Wires::Channel['3']).to receive(:register).with(event, &a_proc)
+      subject.on(event, ['1','2','3'].map{|s| Wires::Channel[s]}, &a_proc)
+    end
+    
+    it "forwards to multiple Channels when given a channel name array" do
+      expect(Wires::Channel['1']).to receive(:register).with(event, &a_proc)
+      expect(Wires::Channel['2']).to receive(:register).with(event, &a_proc)
+      expect(Wires::Channel['3']).to receive(:register).with(event, &a_proc)
+      subject.on(event, ['1','2','3'], &a_proc)
     end
   end
   
   describe "sync_on" do
-    it "forwards to Channel#sync" do
-      extend Wires::Convenience
-      event, channel = :test[], 'test_chan'
-      kwargs = {timeout:55}
-      block = Proc.new{}
-      
-      Wires::Channel[channel].should_receive(:sync_on)
-                             .with(event, **kwargs, &block)
-      sync_on event, channel, **kwargs, &block
+    let(:kwargs) { {timeout:55} }
+    
+    it "forwards to Channel[self].sync_on by default" do
+      expect(Wires::Channel[subject]).to receive(:sync_on).with(event, **kwargs, &a_proc)
+      subject.sync_on(event, **kwargs, &a_proc)
     end
     
-    it "forwards to Channel#sync with self as the default channel" do
-      extend Wires::Convenience
-      event, channel = :test[], 'test_chan'
-      kwargs = {}
-      block = Proc.new{}
-      
-      Wires::Channel[self].should_receive(:sync_on)
-                          .with(event, **kwargs, &block)
-      sync_on event, **kwargs, &block
+    it "forwards to the Channel when given the channel object" do
+      expect(channel).to receive(:sync_on).with(event, kwargs, &a_proc)
+      subject.sync_on(event, channel, **kwargs, &a_proc)
+    end
+    
+    it "forwards to the Channel when given the channel name" do
+      expect(channel).to receive(:sync_on).with(event, kwargs, &a_proc)
+      subject.sync_on(event, channel_name, **kwargs, &a_proc)
+    end
+  end
+  
+  describe "#fire" do
+    it "forwards to Channel[self].fire by default" do
+      expect(Wires::Channel[subject]).to receive(:fire).with(event, {})
+      subject.fire(event)
+    end
+    
+    it "forwards to the Channel when given the channel object" do
+      expect(channel).to receive(:fire).with(event, {})
+      subject.fire(event, channel)
+    end
+    
+    it "forwards to the Channel when given the channel name" do
+      expect(channel).to receive(:fire).with(event, {})
+      subject.fire(event, channel_name)
+    end
+    
+    it "forwards relevant keyword arguments" do
+      kwargs = { blocking:true, parallel:true }
+      expect(channel).to receive(:fire).with(event, kwargs)
+      subject.fire(event, channel_name, **kwargs)
+    end
+  end
+  
+  describe "#fire!" do
+    it "forwards to #fire, adding the blocking:true keyword arg" do
+      expect(subject).to receive(:fire).with(1,2,3,{other:55,blocking:true})
+      subject.fire!(1,2,3,other:55)
+    end
+    
+    it "leaves the blocking keyword arg alone if it was specified" do
+      expect(subject).to receive(:fire).with(1,2,3,{other:55,blocking:false})
+      subject.fire!(1,2,3,other:55,blocking:false)
     end
   end
 end
