837220641d3d2598c0c318c8f72dd285aabd7adc
Joe McIlvain <joe.eli.mac@gmail.com>
2014-04-03 20:25:18 -0700

    Basic code block internalization and execution
    --HG--
    branch : future

--- lib/wires/base/future.rb
+++ lib/wires/base/future.rb
@@ -3,6 +3,19 @@ module Wires.current_network::Namespace
   
   class Future
     
+    attr_reader :codeblock
+    
+    def initialize(&block)
+      raise ArgumentError, "Future must be instantiated with a block" \
+        unless block
+      
+      @codeblock = block
+    end
+    
+    def execute *args, &block
+      @codeblock.call *args, &block
+    end
+    
   end
   
 end
--- spec/base/future_spec.rb
+++ spec/base/future_spec.rb
@@ -6,8 +6,29 @@ require 'spec_helper'
 
 describe Wires::Future, iso:true do
   
-  subject { Wires::Future.new }
+  subject { Wires::Future.new &codeblock }
   
-  specify {  }
+  let(:codeblock) { Proc.new { return_value } }
+  let(:return_value) { Object.new }
+  let(:call_args) { [1, 2, 3, a:4, b:5] }
+  let(:call_block) { Proc.new { } }
+  
+  
+  its(:codeblock) { should eq codeblock }
+  
+  it "raises an ArgumentError if instantiated without a block" do
+    expect { Wires::Future.new }.to raise_error ArgumentError, /block/
+  end
+  
+  its(:execute) { should eq codeblock.call }
+  
+  it "can send arguments to the block with execute" do
+    codeblock.should_receive(:call) { |*a, &b|
+      a.should eq call_args
+      b.should eq call_block
+      return_value
+    }
+    subject.execute(*call_args, &call_block).should eq return_value
+  end
   
 end
