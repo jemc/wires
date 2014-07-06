3c89ebfd409c66915313e1de2a1279067f438a8a
Joe McIlvain <joe.eli.mac@gmail.com>
2014-07-06 11:06:18 -0700

    Start of basic test for ThreadPool
    --HG--
    extra : amend_source : 64a4607fa0b3b775f1aec48c521657c63b4ae4f4

--- spec/util/thread_pool_spec.rb
+++ spec/util/thread_pool_spec.rb
@@ -2,12 +2,32 @@
 require 'spec_helper'
 
 
-describe Wires::Util::ThreadPool do
+describe Wires::Util::ThreadPool, iso:true do
   
   subject { Wires::Util::ThreadPool.new min_threads, max_threads }
   let(:min_threads) { 3 }
   let(:max_threads) { 5 }
   
-  it { should be }
+  its(:min) { should eq min_threads }
+  its(:max) { should eq max_threads }
+  
+  it "creates the minimum number of threads on creation" do
+    original_count = Thread.list.count
+    
+    subject
+    
+    Thread.list.count.should eq original_count + min_threads
+  end
+  
+  it "creates no more than the maximum number of threads" do
+    release = false
+    original_count = Thread.list.count
+    
+    (max_threads+3).times { subject.process { sleep 0 until release } }
+    
+    Thread.list.count.should eq original_count + max_threads
+    release = true
+    subject.wait_done
+  end
   
 end
